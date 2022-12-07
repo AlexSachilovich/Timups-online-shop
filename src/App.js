de5291a9a2ec1a7de9f6418170b57b34b444d29c
Schilovich Alexander <sachilovich.alex@gmail.com>
2022-12-07 14:06:00 +0300

    create header

--- src/App.js
+++ src/App.js
@@ -1,2 +1,11 @@
+import { Component } from "./core";
+import "./components";
+export class App extends Component {
+  render() {
+    return `
+       <t-header></t-header>
+        `;
+  }
+}
 
-
+customElements.define("my-app", App);
